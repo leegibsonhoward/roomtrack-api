a5197c8ed48052848310ad2c9733d920cedf6508
Lee Howard <1238330+leegibsonhoward@users.noreply.github.com>
2026-09-20 01:43:05 -0700

    Add endpoint to create asset

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+// memory database
+let rooms = [
+  {id: 1, number: "101", assets: [] },
+  {id: 2, number: "102", assets: [] }
+];
+
+// 
+// Routes //////////////////////////////////////////
+//
 app.get('/', (req, res) => {
   res.json({
     status: "Ok",
@@ -11,11 +20,45 @@ app.get('/', (req, res) => {
   })
 });
 
+// list all rooms
 app.get('/rooms', (req, res) => {
-  res.json([
-    {id: 1, number: "101"},
-    {id: 2, number: "102"}
-  ])
+  res.json(rooms);
+});
+
+// create asset for room by ID
+app.post('/rooms/:roomId/assets', (req, res) => {
+  
+  let result = rooms.find((room => room.id == req.params.roomId));
+  console.log(result);
+
+    const roomId = req.params.roomId;
+    let assets = result.assets;
+
+    // check if room id exists in database
+    if (!roomId) {
+        return res.status(400).json({ 
+            success: false, 
+            message: 'Room is required' 
+        });
+    }
+
+    // create a new asset object
+    const newAsset = {
+        id:  + 1, // generate a mock incremental ID
+        name: "t.v"
+    };
+
+    // save the new asset into mock database
+    assets.push(newAsset);
+
+    console.log(assets);
+
+    // respond with status code 201 (Created) and return the new item
+    res.status(201).json({
+        success: true,
+        message: 'Asset created successfully',
+        data: newAsset
+    });
 });
 
 app.listen(port, () => {
